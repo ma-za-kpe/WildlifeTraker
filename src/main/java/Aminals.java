dc060de8547bbb1bddf68c8127d1f2d68ca4a8c6
ma-za-kpe <makpalyy@gmail.com>
2019-04-28 16:27:34 +0200

    add the basic tests in the AnimalTest file

--- src/main/java/Aminals.java
+++ src/main/java/Aminals.java
@@ -1,3 +1,80 @@
-public class Aminals {
+import org.sql2o.Connection;
 
+import java.util.List;
+
+public class Aminals extends Animal{
+
+    public Aminals(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getId() {
+        return super.getId();
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public boolean equals(Object otherAnimal){
+        if (!(otherAnimal instanceof Aminals)) {
+            return false;
+        } else {
+            Aminals newAnimal = (Aminals) otherAnimal;
+            return this.getName().equals(newAnimal.getName());
+        }
+    }
+
+    //save animal into db
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name) VALUES (:name)";
+                    this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    //get all animals
+    public static List<Aminals> all() {
+        String sql = "SELECT * FROM animals";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Aminals.class);
+        }
+    }
+    //find animals by id
+    public static Aminals find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM animals where id=:id";
+            Aminals aminals = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Aminals.class);
+            return aminals;
+        }
+    }
+
+    //update an aminal
+    public void update(String name) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "UPDATE animals SET name = :name WHERE id = :id";
+            con.createQuery(sql)
+                    .addParameter("name", name)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+
+    //delete animal
+    public void delete() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM animals WHERE id = :id;";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
 }
--- src/test/java/AminalsTest.java
+++ src/test/java/AminalsTest.java
@@ -1,4 +1,5 @@
 import org.junit.Rule;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -7,4 +8,71 @@ public class AminalsTest {
     @Rule
     public DatabaseRule database = new DatabaseRule();
 
+    @Test
+    public void aminal_instantiatesCorrectly_true() {
+        Aminals aminals= new Aminals("lion");
+        assertEquals(true, aminals instanceof Aminals);
+    }
+
+    @Test
+    public void equals_returnsTrueIfNameAreSame_true() {
+        Aminals aminals = new Aminals("lion");
+        Aminals aminals1 = new Aminals("lion");
+        assertTrue(aminals.equals(aminals1));
+    }
+
+    @Test
+    public void save_insertsObjectIntoDatabase_Wildlife_Tracker() {
+        Aminals testAnimal= new Aminals("lion");
+        testAnimal.save();
+        assertTrue(Aminals.all().get(0).equals(testAnimal));
+    }
+
+    //return all instances of animal
+    @Test
+    public void all_returnsAllInstancesOfPerson_true() {
+        Aminals firstAnimal = new Aminals("lion");
+        firstAnimal.save();
+        Aminals secondAminals = new Aminals("kangaroo");
+        secondAminals.save();
+        assertEquals(true, Aminals.all().get(0).equals(firstAnimal));
+        assertEquals(true, Aminals.all().get(1).equals(secondAminals));
+    }
+
+    //saving our ids form the db to our classes
+    @Test
+    public void save_assignsIdToObject() {
+        Aminals testAminals= new Aminals("Henry");
+        testAminals.save();
+        Aminals savedAminals = Aminals.all().get(0);
+        assertEquals(testAminals.getId(), savedAminals.getId());
+    }
+
+    //find animals based on their id
+    @Test
+    public void find_returnsPersonWithSameId_secondPerson() {
+        Aminals firstAminals= new Aminals("Henry");
+        firstAminals.save();
+        Aminals secondAminals = new Aminals("Harriet");
+        secondAminals.save();
+        assertEquals(Aminals.find(secondAminals.getId()), secondAminals);
+    }
+
+    //update animal
+    @Test
+    public void update_updatesAnimalName_true() {
+        Aminals myAminals= new Aminals("lion");
+        myAminals.save();
+        myAminals.update("elephant");
+        assertEquals("elephant", Aminals.find(myAminals.getId()).getName());
+    }
+
+    @Test
+    public void delete_deletesAnimal_true() {
+        Aminals myAminals = new Aminals("lion");
+        myAminals.save();
+        int myAminalsId = myAminals.getId();
+        myAminals.delete();
+        assertEquals(null, Aminals.find(myAminalsId));
+    }
 }
