8d0c93f01bc9694fd501a88d94c82041cbfcc6d6
ma-za-kpe <makpalyy@gmail.com>
2019-04-28 15:41:15 +0200

    modified DatabaseRule.java file

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,7 +1,20 @@
+import org.junit.rules.ExternalResource;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
-public class DatabaseRule {
+public class DatabaseRule extends ExternalResource {
 
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "maku", "maku");
+    }
+
+    @Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deletePersonsQuery = "DELETE FROM animals *;";
+            con.createQuery(deletePersonsQuery).executeUpdate();
+        }
+    }
 
 }
