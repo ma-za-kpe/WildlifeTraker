197d5b46f3513a1c037557b404acfb2324b046ec
ma-za-kpe <makpalyy@gmail.com>
2019-04-28 16:32:00 +0200

    added the constants in teh abstract animal.java class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -3,6 +3,18 @@ public abstract class Animal {
     public int id;
     public String name;
 
+    //CONSTANTS
+    public String healthLevel;
+    public String ageLevel;
+
+    public static final String MAX_HEALTH_LEVEL = "healthy";
+    public static final String MEDIUM_HEALTH_LEVEL = "okay";
+    public static final String MIN_HEALTH_LEVEL = "ill";
+
+    public static final String MAX_AGE_LEVEL = "adult";
+    public static final String MEDIUM_AGE_LEVEL = "young";
+    public static final String MIN_AGE_LEVEL = "mewborn";
+
     public int getId() {
         return id;
     }
