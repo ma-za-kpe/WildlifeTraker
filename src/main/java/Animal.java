283326cefc0e30df3bed255ac9afbadb93c9d60c
ma-za-kpe <makpalyy@gmail.com>
2019-04-28 15:36:28 +0200

    a]created and modified tha abstract Animal.java class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,2 +1,13 @@
-public class Animal {
+public abstract class Animal {
+
+    public int id;
+    public String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
